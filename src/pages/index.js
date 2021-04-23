a1f3e7e97774a90abcb576eee19aba331009a641
DustinF <57939064+hackistic@users.noreply.github.com>
2021-04-23 04:04:07 -0500

    Update index.js
    Changed Title and <p> and <b?

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Warning What you May Learn Hear will Shock you.</h1>
+    <p>This is unfiltered open source investigation into the TRUTH</p>
+    <p>and possibly a litte more....</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
